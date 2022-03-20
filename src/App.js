4b0d1c2cd69a2876c2924a104bdf23a93cc843b1
Ddxx98 <deexith2016@gmail.com>
2022-03-20 21:37:08 +0530

    deekshith-footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/footer'
 import React , { useState } from "react"
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   
   return (
     <div className="App">
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../assets/footer.css"
+
+function Footer(props) {
+  return (
+    <footer className='footer-component'>
+      <p>2022</p>
+      <div className='footer-circle'>C</div>
+      <p>Laundry</p>
+    </footer>
+  )
+}
+
+export default Footer
